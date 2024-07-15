33744eb34ffba726ab34bae5dfdac86145a4ed9f
audiopoa <consultoria@audiopoa.com>
2024-07-15 12:47:07 -0300

    add a icon whatsapp

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -19,7 +18,7 @@ export default function Home() {
           href="https://wa.me/5551993312020?text=Ol%C3%A1.%20Encontrei%20a%20box%20cristo%20no%20google!%20Preciso%20de%20algumas%20informa%C3%A7%C3%B5es..."
           className="inline-flex items-center justify-center px-3 py-2 text-xs font-semibold text-white bg-[#25D366] rounded-lg shadow hover:bg-green-500"
         >
-          <FontAwesomeIcon icon="fa-brands fa-whatsapp" size="" />
+          <FaWhatsapp className="text-2xl mr-2" />
           Entre em contato pelo WhatsApp
         </a>
       </div>
